637707a387b33f2bb1defe0e1a87b5e91d35bff9
William Clément <wiyam@icloud.com>
2016-05-04 19:09:48 +0200

    Added files via upload

---
+++ about.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+
+<!-- title and meta -->
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+<meta name="description" content="" />
+<title>Yeep:: LE BLOG COMMUNAUTAIRE</title>
+    
+<!-- css -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Bitter:400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/font-awesome/font-awesome.css">
+<link rel="stylesheet" href="css/base.css" />
+<link rel="stylesheet" href="css/style.css" />
+
+</head>
+
+
+
+<body>
+
+<nav class="menu slide-menu-left">
+       <ul>
+        <li><button class="close-menu"><h2>=</h2></button></li>
+        <li><a href="#">Yeep</a></li>
+		<li><a href="yeepr.php">Yeep acces redacteur</a></li>
+		<li><a href="about.php">A propos</a></li>
+		<li><a href="room.php">Tchat Room</a></li>
+    </ul>
+</nav><!-- /slide menu left -->
+
+<nav class="menu slide-menu-right">
+    <ul>
+        <li><button class="close-menu">Close &darr;</button></li>
+        <li><a href="http://native.chez.com/blog/">Native Open DATA</a></li>
+        <li><a href="http://native.chez.com/tolki/">Tolki-THE GAME</a></li>
+        <li><a href="http://native.chez.com/headr">HEADr</a></li>
+        <li><a href="http://qappl.cla.fr/">QAPPL</a></li>
+    </ul>
+</nav><!-- /slide menu right -->
+
+<nav class="menu slide-menu-top">
+    <ul>
+        <li><button class="close-menu">Close &darr;</button></li>
+        <li><a href="http://native.chez.com/blog/">Native Open DATA</a></li>
+        <li><a href="http://native.chez.com/tolki/">Tolki-THE GAME</a></li>
+        <li><a href="http://native.chez.com/headr">HEADr</a></li>
+        <li><a href="http://qappl.cla.fr/">QAPPL</a></li>
+    </ul>
+</nav><!-- /slide menu top -->
+
+<nav class="menu slide-menu-bottom">
+    <ul>
+        <li><button class="close-menu">Close &darr;</button></li>
+        <li><a href="http://native.chez.com/blog/">Native Open DATA</a></li>
+        <li><a href="http://native.chez.com/tolki/">Tolki-THE GAME</a></li>
+        <li><a href="http://native.chez.com/headr">HEADr</a></li>
+        <li><a href="http://qappl.cla.fr/">QAPPL</a></li>
+    </ul>
+</nav><!-- /slide menu bottom -->
+
+<nav class="menu push-menu-left">
+    <ul>
+        <li><button class="close-menu">Close &darr;</button></li>
+        <li><a href="http://native.chez.com/blog/">Native Open DATA</a></li>
+        <li><a href="http://native.chez.com/tolki/">Tolki-THE GAME</a></li>
+        <li><a href="http://native.chez.com/headr">HEADr</a></li>
+        <li><a href="http://qappl.cla.fr/">QAPPL</a></li>
+    </ul>
+</nav><!-- /push menu left -->
+
+<nav class="menu push-menu-right">
+    <ul>
+        <li><button class="close-menu">Close &darr;</button></li>
+        <li><a href="http://native.chez.com/blog/">Native Open DATA</a></li>
+        <li><a href="http://native.chez.com/tolki/">Tolki-THE GAME</a></li>
+        <li><a href="http://native.chez.com/headr">HEADr</a></li>
+        <li><a href="http://qappl.cla.fr/">QAPPL</a></li>
+    </ul>
+</nav><!-- /push menu right -->
+
+<nav class="menu push-menu-top">
+    <ul>
+        <li><button class="close-menu">Close &darr;</button></li>
+        <li><a href="http://native.chez.com/blog/">Native Open DATA</a></li>
+        <li><a href="http://native.chez.com/tolki/">Tolki-THE GAME</a></li>
+        <li><a href="http://native.chez.com/headr">HEADr</a></li>
+        <li><a href="http://qappl.cla.fr/">QAPPL</a></li>
+    </ul>
+</nav><!-- /push menu top -->
+
+<nav class="menu push-menu-bottom">
+    <ul>
+        <li><button class="close-menu">Close &darr;</button></li>
+        <li><a href="http://native.chez.com/blog/">Native Open DATA</a></li>
+        <li><a href="http://native.chez.com/tolki/">Tolki-THE GAME</a></li>
+        <li><a href="http://native.chez.com/headr">HEADr</a></li>
+        <li><a href="http://qappl.cla.fr/">QAPPL</a></li>
+    </ul>
+</nav><!-- /push menu bottom -->
+
+<div id="wrapper">
+
+    <div class="info-bar">
+        <div class="container">
+            <a href="#" class="icon fa fa-home" data-title="#"></a>
+            <a href="#" class="icon fa fa-arrow-left" data-title="#"></a>
+            <!-- <a href="" class="icon fa fa-arrow-right" data-title="Next Demo"></a> -->
+            <a href="#" class="icon fa fa-download" data-title="#"></a>
+            <a href="#" class="icon fa fa-folder-open" data-title="#"></a>
+        </div>
+    </div><!-- /.info-bar -->
+  
+<center>  
+    <header>
+        <div class="branding">
+            <div class="container clearfix">
+                <div class="logo">
+                    <a href="../yeep/">
+                        <img src="img/logo.png" alt="Yeep" />
+                    </a>
+                </div>
+                <div class="social">
+                    
+                    <a href="https://twitter.com/wiyam_" target="_blank" class="twitter">twitter</a>
+                    
+                </div>
+            </div>
+        </div><!-- /.branding -->
+        <div class="site-title">
+            <div class="container">
+                <h1>Yeep </h1>
+            </div>
+        </div>
+    </header>
+</center>
+
+    <div id="main">
+        <div class="container">
+            
+            <div class="buttons">
+                <button class="nav-toggler toggle-slide-left"><h2>= DISCOVER</h2></button>
+				<div id="id_div_1" style="display:none;">
+                <button class="nav-toggler toggle-slide-right"></button>
+                <button class="nav-toggler toggle-slide-top"></button>
+                <button class="nav-toggler toggle-slide-bottom"></button><br>
+                <button class="nav-toggler toggle-push-left"></button>
+                <button class="nav-toggler toggle-push-right"></button>
+                <button class="nav-toggler toggle-push-top"></button>
+                <button class="nav-toggler toggle-push-bottom"></button>
+				</div>
+            </div><!-- /buttons -->
+
+            <section class="content">
+                <h1>BLOG</h1>
+<p>Yeep est un blog propulse par Yeepr, un CMS cree par Will "wiyam" C.</p>
+<p>Ce blog est a la fois experimentale, mais les post postees sur ce blog sont egalement crees pour proposer un contenu intuitif portant sur differentes categories: internet, informatique, qui sont mes passion.</p>
+<p>Vous pouvez me suivre sur Twitter: @wiyam_</p>
+<p>Bonne navigation sur Yeep!</p>
+<p>PS: je sais que les accents ne s'affichent pas, je suis sur le point de resoudre ce probleme.</p>
+            </section>
+
+        </div>
+    </div><!-- #main -->
+
+
+    <footer>
+        <div class="container">
+            <div class="clearfix">
+                <aside>
+                    <ul>
+                        <li><a href="http://www.twitter.com/wiyam_/">TweetME</a></li>
+                        
+                    </ul>
+                </aside>
+                <aside>
+                    <ul>
+                        <li><a href="http://native.chez.com/about.html"></a></li>
+                        
+                    </ul>
+                </aside>
+            </div>
+            <div class="copyright">
+                <span>
+                     Produced by WAC with LOVE<br>
+                    <a href="#" target="_blank">#</a>
+                </span>
+            </div>
+        </div>
+    </footer><!-- /footer -->
+
+</div><!-- /#wrapper -->
+
+<!-- js -->
+<script src="js/classie.js"></script>
+<script src="js/nav.js"></script>
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34160351-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+
+</body>
+</html>
---
+++ yeepr.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+	<head>
+		<title>YEEPr</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="styles.css" type="text/css" charset="utf-8" />
+	</head>
+	<body>
+<center>
+	<div id="toolbar">
+	<a href="../yeep/"><img
+style="border: 0px solid ; width: 372px; height: 210px;" alt=""
+src="http://native.chez.com/yeep/headr.png"></a>
+</div>
+</center>
+<center>
+<div id="content">		
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>.</p>
+		<p>Board partcipatif</p>
+		<p>Partager votre savoir avec des collaborateurs</p>
+		<p>Un espace de travail ou de detente</p>
+		<p>Un espace securise et unique reserve pour un groupe de pair, ou a un cercle strict de connaissance</p>
+		<p>Vous seul connaissez le mot de passe de votre acces redacteur</p>
+		<p><em>YEEPr est un board participatif permettant a des groupes d'utilisateurs de partager des informations et des donnees.</em></p>
+		<p><em>Veuillez entrer le mot de passe pour acceder au systeme</em></p>
+		<form action="home.php" method="post">
+			<p>
+			<input type="password" name="mot_de_passe" />
+			<input type="submit" value="Valider" />
+			</p>
+		</form>
+		<p>Initialement cree par William CLEMENT.</p>
+		<p><strong>@wiyam_</strong></p>
+		<p>(L) Yeep</p>
+</div>
+</center>		
+	</body>
+</html>
